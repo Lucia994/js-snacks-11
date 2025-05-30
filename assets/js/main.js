9ab08ce2aa97954aba762685482efb80f6b08701
Lucia994 <l.summa94@gmail.com>
2025-05-30 11:01:35 +0200

    Complete JavaScript code

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+/*Traccia: L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore*/
+
+//Steps
+// Step -1 Richiesta Utente primo numero 
+const usernumber1 = Number(prompt("Inserisci primo numero"));
+
+
+// Step -2 Richiesta Utente primo numero 
+const usernumber2 = Number(prompt("Inserisci secondo numero"));
+
+// Step 3 - Confronto e stampa del maggiore
+if (firstnumber > secondnumber) {
+    console.log("Il numero maggiore è:", firstnumber);
+} else if (numero2 > numero1) {
+    console.log("Il numero maggiore è:", secondnumber);
+} else {
+    console.log("I numeri sono uguali.");
+
+}
