5c30559c2365669f336da2a66d0bba56d3aa3952
Lucia994 <l.summa94@gmail.com>
2025-05-30 11:10:49 +0200

    Fix variable naming errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,17 @@ console.log("Hello");
 
 //Steps
 // Step -1 Richiesta Utente primo numero 
-const usernumber1 = Number(prompt("Inserisci primo numero"));
+const userNumber1 = Number(prompt("Inserisci primo numero"));
 
 
-// Step -2 Richiesta Utente primo numero 
-const usernumber2 = Number(prompt("Inserisci secondo numero"));
+// Step -2 Richiesta Utente secondo numero 
+const userNumber2 = Number(prompt("Inserisci secondo numero"));
 
 // Step 3 - Confronto e stampa del maggiore
-if (firstnumber > secondnumber) {
-    console.log("Il numero maggiore è:", firstnumber);
+if ( userNumber1> userNumber2) {
+    console.log("Il numero maggiore è:", userNumber1);
 } else if (numero2 > numero1) {
-    console.log("Il numero maggiore è:", secondnumber);
+    console.log("Il numero maggiore è:", userNumber2);
 } else {
     console.log("I numeri sono uguali.");
 
